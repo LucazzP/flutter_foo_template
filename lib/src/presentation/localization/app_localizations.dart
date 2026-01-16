33d96904dffa76a72107379d8f300f47a95a28a0
LucazzP <lucashpolazzo2013@gmail.com>
2026-01-16 18:18:02 -0300

    Updated flutter version

--- lib/src/presentation/localization/app_localizations.dart
+++ lib/src/presentation/localization/app_localizations.dart
@@ -1,1 +1,27 @@
-export 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter/widgets.dart';
+import 'generated/app_localizations.dart' as l;
+
+extension AppLocalizations on l.AppLocalizations {
+  static late l.AppLocalizations current;
+
+  static l.AppLocalizations of(BuildContext context) => l.AppLocalizations.of(context) ?? current;
+
+  static const LocalizationsDelegate<l.AppLocalizations> delegate = l.AppLocalizations.delegate;
+  static const List<Locale> supportedLocales = l.AppLocalizations.supportedLocales;
+
+  static Future<void> initialize(BuildContext context) async {
+    final locale = Localizations.of<l.AppLocalizations>(context, l.AppLocalizations);
+    if (locale == null) {
+      current = await l.AppLocalizations.delegate.load(Locale('en'));
+    } else {
+      current = locale;
+    }
+  }
+
+  Locale get locale {
+    final splitedLocale = localeName.split('_');
+    final languageCode = splitedLocale[0];
+    final countryCode = splitedLocale.length > 1 ? splitedLocale[1] : '';
+    return Locale(languageCode, countryCode);
+  }
+}
---
+++ lib/src/presentation/localization/generated/app_localizations_pt.dart
@@ -0,0 +1,16 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Portuguese (`pt`).
+class AppLocalizationsPt extends AppLocalizations {
+  AppLocalizationsPt([String locale = 'pt']) : super(locale);
+
+  @override
+  String get appTitle => 'new_flutter_template';
+
+  @override
+  String get ok => 'Ok';
+}
